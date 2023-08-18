21f772b54399407267f49879895aea8abb15a5f8
praneeth <praneeth@gmail.com>
2023-08-18 17:12:33 +0200

    greater.sh added

---
+++ greater.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+number=$1
+if [ $number -gt 100 ] 
+then
+	echo "The number you have entered is $number. This is greater than 100"
+else
+	echo "The number you have entered is $number. This is less than 100"
+fi
+	
