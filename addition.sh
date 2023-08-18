d7d02138662e6622352a0abb4aba1ff397d14bd1
praneeth <praneeth@gmail.com>
2023-08-18 13:31:50 +0200

    addition.sh added

---
+++ addition.sh
@@ -0,0 +1,4 @@
+#1/bin/bash
+number1=$1
+number2=$2
+echo " sum of $number1 and $number2 is $((number1+number2))"
--- name.sh
+++ name.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-x=ramesh
-y=suresh 
-echo " $x:Hi $y, How are you "
-echo " $y:I am fine $x,How bout you "
+x=$1
+y=$2 
+echo "$x:Hi $y, How are you "
+echo "$y:I am fine $x,How bout you "
